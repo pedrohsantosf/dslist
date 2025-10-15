2ffde68b5ae177e23d122c952ff7b07e5ac57f5c
Pedro Santos <106943671+pedrohsantosf@users.noreply.github.com>
2025-10-15 18:20:35 -0300

    Game seed

--- src/main/java/com/santos/dslist/entities/Game.java
+++ src/main/java/com/santos/dslist/entities/Game.java
@@ -13,17 +13,24 @@ import java.util.Objects;
 @Table(name = "tb_game")
 
 public class Game {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @Column(name= "game_year")
     private Integer year;
+
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column (columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column (columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
